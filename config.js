1c79b801aa43bc5e8c0470581da925df630cbcb1
OreOluwa Faseru <orefash@gmail.com>
2022-06-23 20:33:45 +0100

    updated db connection

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const env = process.env;
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
   
-    connectionString: DB_URL,
+    connectionString: env.DB_URL,
     ssl: {    /* <----- Add SSL option */
       rejectUnauthorized: false,
     }
