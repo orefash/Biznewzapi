89a8405b3942820c8938df886c24efcfb0630111
OreOluwa Faseru <orefash@gmail.com>
2022-06-24 13:08:43 +0100

    update db query

--- database/articles.js
+++ database/articles.js
@@ -3,10 +3,11 @@ const { getOffset, emptyOrRows } = require('../dbhelper') ;
 const { listPerPage } = require('../config') ;
 
 const getMultiple = async (page = 1) => {
-  const offset = await getOffset(page, listPerPage);
+  // const offset = await getOffset(page, listPerPage);
   const rows = await query(
-    'SELECT id, title, source, content, publish_date, img_url, url FROM news_articles OFFSET $1 LIMIT $2', 
-    [offset, listPerPage]
+    "SELECT * FROM (SELECT DISTINCT ON (title) * FROM news_articles ORDER BY title, publish_date DESC ) p ORDER BY publish_date DESC LIMIT $1", 
+    [listPerPage]
+    // [offset, listPerPage]
   );
   const data = await emptyOrRows(rows);
 
